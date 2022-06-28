f84040411ad3d5bc0cac23e33cfab2b710cd2b5f
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-06-28 13:14:38 -0500

    bmi state

---
+++ lib/bmi.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BMI extends StatefulWidget {
+  @override
+  State<BMI> createState() => BMIState();
+}
+
+class BMIState extends State<BMI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
