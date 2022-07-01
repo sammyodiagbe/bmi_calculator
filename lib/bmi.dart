a28325d6885c8906c00b4fb382901d82d08ffba6
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-07-01 12:24:16 -0500

    almost done with the first page design and look & feel

--- lib/bmi.dart
+++ lib/bmi.dart
@@ -2,25 +2,9 @@ import 'package:bmi_calculator/bmiBrain.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import "reusableColumn.dart";
+import "constants.dart";
 
 BmiBrain brain = BmiBrain();
-
-const defaultBackgroundColor = Color.fromARGB(255, 54, 58, 59);
-const textTitleStyle = TextStyle(
-  fontSize: 16,
-  color: Colors.grey,
-);
-
-const numbersTextStyle = TextStyle(
-  fontSize: 40,
-  fontWeight: FontWeight.bold,
-  color: Colors.white,
-);
-
-const subTextStyle = TextStyle(
-  fontSize: 14,
-  color: Colors.grey,
-);
 
 class BMI extends StatefulWidget {
   const BMI({Key? key}) : super(key: key);
@@ -36,157 +20,164 @@ class BMIState extends State<BMI> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Padding(
-          padding: EdgeInsets.all(15),
-          child: Column(
-            children: [
-              Expanded(
-                child: Row(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(15),
+                child: Column(
                   children: [
                     Expanded(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: defaultBackgroundColor,
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(
-                              FontAwesomeIcons.mars,
-                              size: 60,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: defaultBackgroundColor,
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    FontAwesomeIcons.mars,
+                                    size: 60,
+                                  ),
+                                  SizedBox(height: 10),
+                                  Text(
+                                    'Male',
+                                    style: TextStyle(fontSize: 18),
+                                  )
+                                ],
+                              ),
                             ),
-                            SizedBox(height: 10),
-                            Text(
-                              'Male',
-                              style: TextStyle(fontSize: 18),
-                            )
-                          ],
-                        ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: defaultBackgroundColor,
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    FontAwesomeIcons.venus,
+                                    size: 60,
+                                  ),
+                                  SizedBox(height: 10),
+                                  Text(
+                                    'Female',
+                                    style: TextStyle(fontSize: 18),
+                                  )
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
                       ),
                     ),
                     SizedBox(
-                      width: 10,
+                      height: 15,
                     ),
                     Expanded(
                       child: Container(
-                        decoration: BoxDecoration(
-                          color: defaultBackgroundColor,
-                        ),
+                        width: double.infinity,
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Icon(
-                              FontAwesomeIcons.venus,
-                              size: 60,
+                            Text(
+                              'HEIGHT',
+                              style: textTitleStyle,
                             ),
                             SizedBox(height: 10),
-                            Text(
-                              'Female',
-                              style: TextStyle(fontSize: 18),
-                            )
-                          ],
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Expanded(
-                child: Container(
-                    width: double.infinity,
-                    // child: Align(
-                    //   alignment: Alignment.center,
-                    //   child: Slider(
-                    //     value: _currentSliderValue,
-                    //     min: 0,
-                    //     max: 100,
-                    //     thumbColor: Colors.pink,
-                    //     activeColor: Colors.pink,
-                    //     inactiveColor: Colors.grey,
-                    //     onChanged: (double value) {
-                    //       setState(() {
-                    //         _currentSliderValue = value;
-                    //       });
-                    //     },
-                    //   ),
-
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'HEIGHT',
-                          style: textTitleStyle,
-                        ),
-                        SizedBox(height: 10),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              _currentSliderValue.toString(),
-                              style: numbersTextStyle,
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  _currentSliderValue.toString(),
+                                  style: numbersTextStyle,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  'cm',
+                                  style: subTextStyle,
+                                )
+                              ],
                             ),
                             SizedBox(
-                              width: 10,
+                              height: 10,
                             ),
-                            Text(
-                              'cm',
-                              style: subTextStyle,
-                            )
+                            Slider(
+                              value: _currentSliderValue.toDouble(),
+                              min: 120,
+                              max: 220,
+                              thumbColor: Colors.pink,
+                              activeColor: Colors.pink,
+                              inactiveColor: Colors.grey,
+                              onChanged: (double value) {
+                                setState(() {
+                                  _currentSliderValue = value.round();
+                                });
+                              },
+                            ),
                           ],
                         ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Slider(
-                          value: _currentSliderValue.toDouble(),
-                          min: 120,
-                          max: 220,
-                          thumbColor: Colors.pink,
-                          activeColor: Colors.pink,
-                          inactiveColor: Colors.grey,
-                          onChanged: (double value) {
-                            setState(() {
-                              _currentSliderValue = value.round();
-                            });
-                          },
-                        ),
-                      ],
-                    ),
-                    decoration: BoxDecoration(
-                      color: defaultBackgroundColor,
-                    )),
-              ),
-              SizedBox(height: 15),
-              Expanded(
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        child: ReusableColumn(columnTitle: 'HEIGHT', action: 1),
                         decoration: BoxDecoration(
                           color: defaultBackgroundColor,
                         ),
                       ),
                     ),
-                    SizedBox(
-                      width: 10,
-                    ),
+                    SizedBox(height: 15),
                     Expanded(
-                      child: Container(
-                        child: ReusableColumn(columnTitle: 'AGE', action: 2),
-                        decoration: BoxDecoration(
-                          color: defaultBackgroundColor,
-                        ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              child: ReusableColumn(
+                                  columnTitle: 'HEIGHT', action: 1),
+                              decoration: BoxDecoration(
+                                color: defaultBackgroundColor,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: Container(
+                              child:
+                                  ReusableColumn(columnTitle: 'AGE', action: 2),
+                              decoration: BoxDecoration(
+                                color: defaultBackgroundColor,
+                              ),
+                            ),
+                          )
+                        ],
                       ),
-                    )
+                    ),
                   ],
                 ),
               ),
-            ],
-          ),
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                top: 15,
+              ),
+              height: 60,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'CALCULATE BMI.',
+                  style: buttonTextStyle,
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
---
+++ lib/constants.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+const defaultBackgroundColor = Color.fromARGB(255, 54, 58, 59);
+const textTitleStyle = TextStyle(
+  fontSize: 16,
+  color: Colors.grey,
+);
+
+const numbersTextStyle = TextStyle(
+  fontSize: 40,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+);
+
+const subTextStyle = TextStyle(
+  fontSize: 14,
+  color: Colors.grey,
+);
+
+const buttonTextStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
